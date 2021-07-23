105621212bf2a1558960a959ed5b1e68194f1c14
Sniper10754 <alessioteti28@gmail.com>
2021-07-23 18:03:31 +0200

    fix package-info.java

--- library/core/src/main/kotlin/module-info.java
+++ library/core/src/main/kotlin/module-info.java
@@ -4,6 +4,7 @@ module Coffey.library.core.main {
     requires org.apache.httpcomponents.httpclient;
     requires org.apache.httpcomponents.httpcore;
     requires klaxon;
+    requires org.apache.commons.io;
 
     exports org.coffey;
 }
